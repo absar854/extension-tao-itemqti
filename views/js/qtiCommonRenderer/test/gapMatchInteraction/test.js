55c6994d592d141fc64d449a2f145afc2d23ef4f
Christophe Noël <christophe@taotesting.com>
2016-04-27 10:16:29 +0200

    first meaningful assertions

--- views/js/qtiCommonRenderer/test/gapMatchInteraction/test.js
+++ views/js/qtiCommonRenderer/test/gapMatchInteraction/test.js
@@ -24,9 +24,33 @@ define([
 
         runner = qtiItemRunner('qti', itemData)
           .on('render', function (){
-              assert.ok(true);
-              
-              QUnit.start();
+              try {
+                  var $choices = $('.choice-area .qti-choice');
+                  var $hotSpots = $('.qti-flow-container .gapmatch-content');
+                  
+                  // assert is selected
+                  $choices.eq(0).trigger("click");
+                  assert.ok($choices.eq(0).hasClass('active'), "is active on click");
+                  $hotSpots.each(function() {
+                      assert.ok($(this).hasClass('empty'), "is marked empty");
+                  });
+                  
+                  
+                  $hotSpots.eq(4).trigger("click");
+                  $choices.eq(1).trigger("click");
+                  $hotSpots.eq(3).trigger("click");
+                  $choices.eq(2).trigger("click");
+                  $hotSpots.eq(2).trigger("click");
+                  $choices.eq(3).trigger("click");
+                  $hotSpots.eq(1).trigger("click");
+                  $choices.eq(4).trigger("click");
+                  $hotSpots.eq(0).trigger("click");
+                  
+                  assert.ok(true);
+                  QUnit.start();
+              } catch (err) {
+                  console.log(err.message);
+              }
           })
           .on('responsechange', function (response){
               $('#response-display').html(JSON.stringify(response, null, 2));
